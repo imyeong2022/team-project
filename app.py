af85c262e2828586c7d2f2690100b5ea3b8172bc
Tinao <symoon1500@gmail.com>
2022-11-09 11:56:04 +0900

    condition 페이지에 DB 띄우기

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def condition():
     for i in data_list:
         i["회사"]
     return render_template('Board/Condition.html', data_list=data_list)
-
+    
 @app.route('/faq')
 def faq():
     return render_template('Board/faq.html')
@@ -117,10 +117,17 @@ def login_proc():
                  return ('password is def')
             else:
                 return ('id not found')         
+    cursor.close()            
     return render_template('Board/index.html')
 
 app.secret_key = 'test_secret_key'
 
+@app.route('/logout_proc')
+def logout_proc():
+    if session['logFlag'] == True:
+        session['logFlag'] = False
+    return render_template('Board/header.html') 
+#    return render_template('Board/index.html') 으로 할시 data_list undefined 나와서, 임시로 header로 연결했습니다.
 
 
 @app.route('/logout_proc') #로그아웃
@@ -153,6 +160,7 @@ def join_proc():
             con.commit()
             session.clear()
             return render_template('Board/login.html')
+            curo         
 
 ##################### END 회원가입관련 ###############  
 
@@ -162,25 +170,15 @@ def join_proc():
 def my_page():
     return render_template('Board/myPage.html')
 
-@app.route('/my_page_proc', methods=['GET','POST'])
-def my_page_proc():
+# @app.route('/condition') #상세검색기능
+# def condition():
+#     return render_template('Board/Condition.html')
+    
+# @app.route('/faq') #상세검색기능
+# def faq():
+#     return render_template('Board/faq.html')
 
-    if request.method == 'POST': #request객체 안에 method 기능있음(자바도 마찬가지).
-        user_id = request.form['user_id'] #키값(html의 name값, 변수명은 같게 만들어 주는게 편하니 습관화)
-        user_pw = request.form['user_pw']
-        user_name = request.form['user_name'] #키값(html의 name값, 변수명은 같게 만들어 주는게 편하니 습관화)
-        user_phone = request.form['user_phone']
-        user_birth = request.form['user_birth']
-        if len(user_pw) == 0:
-            return '에러! 입력되지 않은 값이 있습니다!'
-        else:
-            sql =  'UPDATE MEMBER SET PW=%s, Phone=%s WHERE ID=%s'
-            cursor.execute(sql, (user_pw,user_phone,user_id, ))
-            con.commit()
-            
-            return render_template('Board/login.html')
-
-@app.route('/recent_inquiry_company') #활동내역 (열람기업)
+@app.route('/recent_inquiry_company') #상세검색기능
 def recent_inquiry_company():
     return render_template('Board/r-i-c.html')
 
@@ -190,9 +188,10 @@ def persnal_info_change():
 
 #################### END 마이페이지 ###################
 
+@app.route('/condition') #상세검색기능
+def condition():
+    return render_template('Board/Condition.html')
     
-<<<<<<< HEAD
-=======
 @app.route('/faq') # 질문과 답변
 def faq():
     sql = "SELECT * from faq"
@@ -205,7 +204,12 @@ def faq():
     #print(i)
 
     return render_template('Board/faq.html', faq_list = faq_list, faq_len = faq_len)
->>>>>>> 240302a2e556c550cdb16a5f04426cde46ce83e7
+
+@app.route('/personal-info-change') #회원정보수정
+def persnal_info_change():
+    return render_template('Board/personal-info-change.html')
+
+#################### END 마이페이지 ###################
 
 @app.route('/test') # 질문과 답변
 def company():

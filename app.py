83c3b162009fdc570815e4dc4426cbc7b0ecb8e4
JaeHyunKong <jaehyun_kong94@naver.com>
2022-11-24 20:59:07 +0900

    7e66b2f // cherry-pick

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'kongjh941109@gmail.com'
-app.config['MAIL_PASSWORD'] = 'myhinigkzocytxcd'
+app.config['MAIL_PASSWORD'] = 'snuejrgmmznyneie'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -54,8 +54,10 @@ def accountfind():
     userlist2 = cursor.fetchall()
     print(type(userlist2))
     print(userlist2)
-
+    cursor.close()
     return render_template('Board/account.html')
+    
+
 
 @app.route('/accountfind_proc', methods=['POST'])
 def accountfind_proc():
@@ -71,7 +73,7 @@ def accountfind_proc():
     msg = Message('[잡아라]' +user_name_recive+'님의 아이디를 안내드립니다.', sender='kongjh941109@gmail.com', recipients=['cjsdur@naver.com'])
     msg.body = '회원님 안녕하세요.''회원님의 아이디는 다음과 같습니다.\n' '['+find_userid+ ']\n 앞으로도 더욱 편리한 서비스를 제공하기 위해 최선을 다하겠습니다.'
     mail.send(msg)
-
+    cursor.close()
     return jsonify({'msg': '회원님의 이메일로 아이디를 전송했습니다.'})
 
 
@@ -142,43 +144,43 @@ def condition():
         return render_template('Board/Condition.html',like_checked=like_checked,data_list=data_list,interest_com=interest_com,interest_len=interest_len,user_id=user_id)
 
 ####################################################
-@app.route('/employtest') # 체크박스 test
+
+
+@app.route('/employtest')  # 체크박스 test
 def employtest():
     con = dbcall()
     cursor = con.cursor()
     try:
         with con.cursor() as cursor:
-            
-            s=request.args.get('area') 
-            
+
+            s = request.args.get('area')
+
             p = s.split(',')
             content = ''
             for i in range(len(p)):
-                if (i+1) == len(p):                    
+                if (i+1) == len(p):
                     content += "'" + p[i] + "'"
                 else:
                     content += "'" + p[i] + "',"
-            print('>>>>>>>>>>>>'+ content,type(content))
+            print('>>>>>>>>>>>>' + content, type(content))
 
             print('!!!!!!!!!!!!!!!!'+content)
             
             sql="SELECT * from company_info where `region` in ("+ content +")"
             cursor.execute(sql)
-            rows=cursor.fetchall()
+            rows = cursor.fetchall()
             # print(rows)
 
             for row in rows:
-                print('............',row)
+                print('............', row)
             # return jsonify(rows)
-            return rows       
-    except Exception as e:
-        print(e)
-    # finally:
-        # con.close()
+            return rows        
+    finally:
+        con.close()
         
-########################################################찜하기 기능 ####################################
+########################################################찜하기 기능 
 
-@app.route('/interest_select') ###############.마이페이지 찜리스트 select
+@app.route('/interest_select')  # .마이페이지 찜리스트 select
 def interest_select():
     con = dbcall()
     cursor = con.cursor()
@@ -188,20 +190,20 @@ def interest_select():
     interest_com = cursor.fetchall()
     print(interest_com)
     interest_len = len(interest_com)
-    return render_template('Board/interest_company.html', interest_com=interest_com,interest_len=interest_len)
+    cursor.close()
+    return render_template('Board/interest_company.html', interest_com=interest_com, interest_len=interest_len)
 
 
-
-@app.route('/interest_insert') ###############찜리스트 INSERT,UPDATE(DELETE)
+@app.route('/interest_insert')  # 찜리스트 INSERT,UPDATE(DELETE)
 def interest_insert():
     con = dbcall()
     cursor = con.cursor()
     rs={}
     try:
-        user_id=session['ID']
-        data_id=request.args.get('data_id')
-        sql="SELECT * from like_company where id=%s and data_id=%s"
-        cursor.execute(sql,(user_id,data_id))
+        user_id = session['ID']
+        data_id = request.args.get('data_id')
+        sql = "SELECT * from like_company where id=%s and data_id=%s"
+        cursor.execute(sql, (user_id, data_id))
         like_company_all = cursor.fetchall()
         print('>>>>>>',len(like_company_all))
         if(len(like_company_all)>0):
@@ -229,10 +231,10 @@ def interest_insert():
         else:
             print('like_insert')
             sql = "insert into like_company(ID,data_id,result) values(%s,%s,%s)"
-            cursor.execute(sql,(user_id,data_id,1))
+            cursor.execute(sql, (user_id, data_id, 1))
             con.commit()
-            cnt=cursor.rowcount
-            rs = {'status':cnt}
+            cnt = cursor.rowcount
+            rs = {'status': cnt}
     except Exception as e:
         print(e)
         rs = {'status': 0}
@@ -245,35 +247,35 @@ def interest_insert():
 ################################################################################
 
 
-@app.route('/company/<int:data_id>')  ############ 기업상세페이지
+@app.route('/company/<int:data_id>')  # 기업상세페이지
 def company(data_id):
     con = dbcall()
     cursor = con.cursor()
     sql = "SELECT * from company_info where data_id = %s"
     cursor.execute(sql, (data_id, ))
     data_list = cursor.fetchall()
-    company=data_list[0]['industry']
-    
+    company = data_list[0]['industry']
 
-
-    allcom = "SELECT * FROM COMPANY_INFO where industry = %s" 
-    cursor.execute(allcom,(company,))
+    allcom = "SELECT * FROM COMPANY_INFO where industry = %s"
+    cursor.execute(allcom, (company,))
     all_list = cursor.fetchall()
     return render_template('Board/company.html', data_list=data_list, all_list=all_list)
 
 
-@app.route('/excellence_employment') ###############채용정보페이지
+@app.route('/excellence_employment')  # 채용정보페이지
 def excellence_employment():
     return render_template('Board/excellence_employment.html')
 
 
-@app.route('/trend')  ################# 구직트렌드페이지 
+@app.route('/trend')  # 구직트렌드페이지
 def trends():
     return render_template('Board/trend.html')
 
-@app.route('/events')  ################# 채용정보페이지
+
+@app.route('/events')  # 채용정보페이지
 def events():
     return render_template('Board/event.html')
+
 
 @app.route('/our_company')
 def our_company():
@@ -288,10 +290,9 @@ def data():
     cursor.execute(sql)
     data_list1 = cursor.fetchall()
     data_list1 = data_list1
-    data_list1 = data_list1
     data_list_len = len(data_list1)
     print("인덱스길이data", data_list_len)
-   
+    cursor.close()
     return render_template('Board/data.html', data_list=data_list1)
 
 
@@ -304,7 +305,7 @@ def faq():
     faq_list = cursor.fetchall()
     faq_len = len(faq_list)
     print(faq_len)
-
+    cursor.close()
     return render_template('Board/faq.html', faq_list=faq_list, faq_len=faq_len)
 
 @app.route('/qna') ########### 질문게시판
@@ -314,11 +315,13 @@ def qna():
     sql = "SELECT * from qna"
     cursor.execute(sql)
     qna_list = cursor.fetchall()
+    print(qna_list)
     qna_len = len(qna_list)
     print(qna_len)
     return render_template('Board/qna.html', qna_len=qna_len, qna_list=qna_list)
 
-@app.route('/question') ######### 질문으로 넘기기
+
+@app.route('/question')  # 질문으로 넘기기
 def qnastion():
     if session['logFlag'] == True:
         print("세션확인",session['logFlag'])
@@ -395,10 +398,11 @@ def interest():
     cursor = con.cursor()
     user_id=session['ID']
     sql = "SELECT * from like_company_view where m_id=%s"
-    cursor.execute(sql,(user_id,))
+    cursor.execute(sql, (user_id,))
     interest_com = cursor.fetchall()
     interest_len = len(interest_com)
-    return render_template('Board/interest_company.html', interest_com=interest_com,interest_len=interest_len)
+    cursor.close()
+    return render_template('Board/interest_company.html', interest_com=interest_com, interest_len=interest_len)
 
 ##################### Index ###############
 
@@ -533,6 +537,7 @@ def join_proc():
             sql = 'INSERT INTO member(ID, PW, email, NAME, Phone, BIRTH) VALUES(%s,%s,%s,%s,%s,%s)'
             cursor.execute(sql, (user_id, pw_hash, user_email, user_name, user_phone, user_birth, ))
             con.commit()
+            cursor.close()
             return render_template('Board/login.html', id_list=id_list, id_cnt=id_cnt)
     
 
@@ -563,7 +568,7 @@ def my_page_proc():
             sql = 'UPDATE MEMBER SET PW=%s, Phone=%s WHERE ID=%s'
             cursor.execute(sql, (user_pw, user_phone, user_id, ))
             con.commit()
-
+            cursor.close()
             return render_template('Board/login.html')
 
 

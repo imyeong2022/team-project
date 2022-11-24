2aa4cf13cfbccd7aa1fac9e07fe11a602d146d00
syfirstweb <seoyen95@naver.com>
2022-11-24 11:09:00 +0900

    '일자리 지도' 버튼 구현완료(html 다른 페이지 내 원하는 위치로 이동)

--- static/js/app_conditon.js
+++ static/js/app_conditon.js
@@ -32,7 +32,10 @@ function checkbox_test() {
       let li = data_list[""];
       for (let i = 0; i < data_list.length; i++) {
         li +=
-          '<li class="item"><div class = "box-text"><div class="box-btn-area"><a class="btn btn-default btn-md"><div><span>☆ 관심기업 등록하기</span></div></a><a class="btn btn-default btn-md"><div><span>일자리 지도 보기</span></div></a><!-- 1라인--><span class="grid-sub-blue">' +
+          '<li class="item"><div class = "box-text"><div class="box-btn-area"><a class="btn btn-default btn-md"><div><span>☆ 관심기업 등록하기</span></div></a>'
+          +'<a class="btn btn-default btn-md" href=\'/company/'+
+          data_list[i].data_id + '#location_map' +'\';" style="cursor:pointer">'
+          +'<div><span>일자리 지도 보기</span></div></a><!-- 1라인--><span class="grid-sub-blue">' +
           data_list[i]["industry"] +
           '</span><a onclick="location.href=\'/company/'
           +data_list[i].data_id +'\';" style="cursor:pointer"><p class="grid-title">' +

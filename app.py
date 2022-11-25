7cdb40cf39092ec8e55217022fa018ec09781f77
Tinao <symoon1500@gmail.com>
2022-11-25 17:52:38 +0900

    11/25 url에 지역 넣었을 때 해당하는 데이터 호출함

--- app.py
+++ app.py
@@ -2,24 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request , flash, js
 from flask import request, session
 from flask_mail import Mail, Message
 import pymysql, hashlib, json, re
-# import mysql.connector
 
-host='localhost'
-user='root'
-password='java'
-db='final_test'
-charset='utf8mb4'
-# def main(): #DB Connection 생성
-con = pymysql.connect(host='localhost',
-                                user='root',
-                                password='java',
-                                db='final_test',
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor)
-# sql_all =  "SELECT * from company_info"                              
-cursor = con.cursor() # 커서 생성   
-# con.close()
-###########데이터베이스 접속 전역변수 선언############
 app = Flask(__name__)
 ############ 구글메일(계정찾기 테스트) ################ 사이트 접근과 동시에 메일서버와 인터페이스 하면서 recipients 로 지정된 메일 주소로 이메일을 발송
 # app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -30,18 +13,35 @@ app = Flask(__name__)
 # app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
+def dbcall():
+    con = pymysql.connect(
+    # host='localhost',
+    user='root',
+    password='java',
+    db='final_test',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor)
+    return con
+
+
+
 @app.route('/accountfind')
 def accountfind():
+    con = dbcall()
+    cursor = con.cursor()
     sql = "SELECT ID,email from member"
     cursor.execute(sql)
     userlist2 = cursor.fetchall()
     print(type(userlist2))
     print(userlist2)
-
+    con.close()
     return render_template('Board/account.html')
 
 @app.route('/accountfind_proc', methods=['POST'])
 def accountfind_proc():
+    con = dbcall()
+
+    cursor = con.cursor()
     user_name_recive = request.form['user_name_give']
     user_email_recive = request.form['user_email_give']
     sql = "SELECT ID from member where name = %s and email = %s"
@@ -52,7 +52,7 @@ def accountfind_proc():
     msg = Message('[잡아라]' +user_name_recive+'님의 아이디를 안내드립니다.', sender='kongjh941109@gmail.com', recipients=['cjsdur@naver.com'])
     msg.body = '회원님 안녕하세요.''회원님의 아이디는 다음과 같습니다.\n' '['+find_userid+ ']\n 앞으로도 더욱 편리한 서비스를 제공하기 위해 최선을 다하겠습니다.'
     mail.send(msg)
-
+    con.close()
     return jsonify({'msg': '회원님의 이메일로 아이디를 전송했습니다.'})
 
 
@@ -67,40 +67,102 @@ def passfind():
 
 @app.route('/')
 def home():
+    con = dbcall()
+    cursor = con.cursor()
     sql = "SELECT * from company_info"
     cursor.execute(sql)
     data_list = cursor.fetchall()
     data_list = data_list
-    print("인덱스타입",type(data_list))
+    # print("인덱스타입",type(data_list))
     data_list_len = len(data_list)
     print("인덱스길이", data_list_len)
 
+    # area_list =data_list[0]['region']
+    # print(area_list)
+    # allcom = "SELECT * FROM COMPANY_INFO where industry = %s" 
+    # cursor.execute(allcom,(area_list,))
+    # all_list = cursor.fetchall()
+    cursor.close()
     return render_template('Board/index.html', data_list=data_list)
-    
+    # return render_template('Board/index.html', data_list=data_list, area_list=area_list, all_list=all_list)
+
 
 # --------------------------메뉴-----------------------------------
 
 @app.route('/condition')  # 조건으로 찾기 - 기업정보
 def condition():
-    # print('여기')
-    # try :
-    #     with con.cursor() as cursor:
-    #         sql = "SELECT * from company_info"
-    #         cursor.execute(sql)
-    #         data_list = cursor.fetchall()
-    #         return render_template('Board/Condition.html', data_list=data_list)
-    # finally :
-    #     con.close()
-     with con.cursor() as cursor:
-        sql = "SELECT * from company_info"
+    con = dbcall()
+    cursor = con.cursor()
+    sql = "SELECT * from company_info"
+    cursor.execute(sql)
+    data_list = cursor.fetchall()
+    cursor.close()
+ 
+    region = request.args.get('areatag')
+    print(region)
+    p = region.split(',')
+    print(p)
+    content = ''
+    for i in range(len(p)):
+            if (i+1) == len(p):                    
+                content += "'" + p[i] + "'"
+            else:
+                content += "'" + p[i] + "',"
+    try:
+        con = dbcall()
+        cursor = con.cursor()
+        sql="SELECT * from company_info where `region` in ("+ content +")"
         cursor.execute(sql)
-        data_list = cursor.fetchall()
-        # cursor.close()
-        return render_template('Board/Condition.html', data_list=data_list)
+        rows = cursor.fetchall()
+        for row in rows:
+            print('............',row)
+        
+        # print(rows)
+        print('화이팅!')
+        data_list = rows
+    except Exception as e:
+        print(e)        
+    finally:
+        # con.close()
+        cursor.close()
     
+    return render_template('Board/Condition.html', data_list=data_list, rows=rows, region=region, datatype="json") 
+
+    # return render_template('Board/Condition.html', data_list=data_list)
+
+def searchIndex():
+    region = request.args.get('areatag', type = str)
+    print(region)
+    p = region.split(',')
+    print(p)
+    content = ''
+    for i in range(len(p)):
+            if (i+1) == len(p):                    
+                content += "'" + p[i] + "'"
+            else:
+                content += "'" + p[i] + "',"
+    try:
+        con = dbcall()
+        cursor = con.cursor()
+        sql="SELECT * from company_info where `region` in ("+ content +")"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            print('............',row)
+        
+        # print(rows)
+        print('화이팅!')
+        data_list = rows
+    except Exception as e:
+        print(e)        
+    finally:
+        # con.close()
+        cursor.close()
+        return render_template('Board/Condition.html', data_list=data_list, rows=rows, region=region, datatype="json") 
+
 @app.route('/search')  # 조건으로 찾기 - 기업정보
 def search():
-
+    con = dbcall()
     with con.cursor() as cursor:  
         s=request.args.get('area') 
         p = s.split(',')
@@ -113,34 +175,44 @@ def search():
         print('>>>>>>>>>>>>'+ content,type(content))
         print('!!!!!!!!!!!!!!!!'+content)
         sql="SELECT * from company_info where `region` in ("+ content +")"
-        cursor = con.execute(sql)
+        cursor.execute(sql)
         rows=cursor.fetchall()
         for row in rows:
             print('............',row)
+        
+        con.close()
         return rows
 
-    # try:
-    #     with con.cursor() as cursor:  
-    #         s=request.args.get('area') 
-    #         p = s.split(',')
-    #         content = ''
-    #         for i in range(len(p)):
-    #             if (i+1) == len(p):                    
-    #                 content += "'" + p[i] + "'"
-    #             else:
-    #                 content += "'" + p[i] + "',"
-    #         print('>>>>>>>>>>>>'+ content,type(content))
-    #         print('!!!!!!!!!!!!!!!!'+content)
-    #         sql="SELECT * from company_info where `region` in ("+ content +")"
-    #         cursor.execute(sql)
-    #         rows=cursor.fetchall()
-    #         for row in rows:
-    #             print('............',row)
-    #         return rows
-    # finally:
-    #     con.close()
-
-
+@app.route('/searchIndex')  # INDEX => 조건으로 찾기
+# def searchIndex():
+#     region = request.args.get('areatag', type = str)
+#     print(region)
+#     p = region.split(',')
+#     print(p)
+#     content = ''
+#     for i in range(len(p)):
+#             if (i+1) == len(p):                    
+#                 content += "'" + p[i] + "'"
+#             else:
+#                 content += "'" + p[i] + "',"
+#     try:
+#         con = dbcall()
+#         cursor = con.cursor()
+#         sql="SELECT * from company_info where `region` in ("+ content +")"
+#         cursor.execute(sql)
+#         rows = cursor.fetchall()
+#         for row in rows:
+#             print('............',row)
+        
+#         # print(rows)
+#         print('화이팅!')
+#         data_list = rows
+#     except Exception as e:
+#         print(e)        
+#     finally:
+#         # con.close()
+#         cursor.close()
+#         return render_template('Board/Condition.html', data_list=data_list, rows=rows, region=region, datatype="json") 
 
 @app.route('/excellence_employment')
 def excellence_employment():
@@ -150,8 +222,14 @@ def excellence_employment():
 @app.route('/employtest') # 체크박스 test
 def employtest():
     try:
-        with con.cursor() as cursor:
-            
+        con = pymysql.connect(
+        user='root',
+        password='java',
+        db='final_test',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+
+        with con.cursor() as cursor:   
             s=request.args.get('area') 
             p = s.split(',')
             content = ''
@@ -186,13 +264,13 @@ def employtest():
 #     interest_len = len(interest_com)
 #     return render_template('Board/interest_company.html', interest_com=interest_com,interest_len=interest_len)
 
-
-
 ########################################################
-
 
 @app.route('/company/<int:data_id>')  ############ 기업상세페이지
 def company(data_id):
+    con = dbcall()
+    cursor = con.cursor()
+
     sql = "SELECT * from company_info where data_id = %s"
     cursor.execute(sql, (data_id,))
     data_list = cursor.fetchall()
@@ -203,6 +281,7 @@ def company(data_id):
     allcom = "SELECT * FROM COMPANY_INFO where industry = %s" 
     cursor.execute(allcom,(recommendation,))
     all_list = cursor.fetchall()
+    con.close()
     return render_template('Board/company.html', data_list=data_list, all_list=all_list)
 
 @app.route('/jobs')  ################# 채용정보페이지
@@ -212,21 +291,26 @@ def jobs():
 
 @app.route('/faq')  ############## 질문과 답변
 def faq():
+    con = dbcall()
+    cursor = con.cursor()
     sql = "SELECT * from faq"
     cursor.execute(sql)
     faq_list = cursor.fetchall()
     faq_len = len(faq_list)
     print(faq_len)
-
+    con.close()
     return render_template('Board/faq.html', faq_list=faq_list, faq_len=faq_len)
 
 @app.route('/qna') ########### 질문게시판
 def qna():
+    con = dbcall()
+    cursor = con.cursor()
     sql = "SELECT * from qna"
     cursor.execute(sql)
     qna_list = cursor.fetchall()
     qna_len = len(qna_list)
     print(qna_len)
+    con.close()
     return render_template('Board/qna.html', qna_len=qna_len, qna_list=qna_list)
 
 @app.route('/question') ######### 질문으로 넘기기
@@ -239,6 +323,9 @@ def qnastion():
         
 @app.route('/question_proc', methods=['POST']) ########## 질문페이지
 def qustion_proc():
+    con = dbcall()
+    cursor = con.cursor()
+
     if request.method == 'POST':
         print(session['ID'])
         ID = request.form['ID']
@@ -250,8 +337,7 @@ def qustion_proc():
     qna_list = cursor.fetchall()
     qna_len = len(qna_list)
     print(qna_len)
-
-
+    con.close()
     return redirect('/qna')
 
 ##################### Index ###############
@@ -260,10 +346,21 @@ def qustion_proc():
 
 @app.route('/login_form_get')
 def login_form_get():
+    con = dbcall()
+    cursor = con.cursor()
+    con.close()
     return render_template('Board/login.html')
 
 @app.route('/login_proc', methods=['POST'])
 def login_proc():
+    con = pymysql.connect(
+                # host='localhost',
+                user='root',
+                password='java',
+                db='final_test',
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor)
+    cursor = con.cursor()
 
     if request.method == 'POST':  # request객체 안에 method 기능있음(자바도 마찬가지).
         # 키값(html의 name값, 변수명은 같게 만들어 주는게 편하니 습관화)
@@ -298,7 +395,10 @@ app.secret_key = 'test_secret_key'
 
 @app.route('/logout_proc')  # 로그아웃
 def logout_proc():
+    con = dbcall()
+    cursor = con.cursor()
     session.clear()  # 세션날림
+    con.close()
     return redirect('/')
     
 ##################### END 로그인관련 ###############
@@ -309,27 +409,33 @@ def logout_proc():
 
 @app.route('/join_form_get')
 def join_form_get():
-        idcheck = 'select ID from member'
-        cursor.execute(idcheck)
-        id_list = cursor.fetchall()
-        print("id_list값",id_list)
-        print("id_list의타입",type(id_list))
-        return render_template('Board/join.html' , data=json.dumps(id_list, ensure_ascii=False))
+    con = dbcall()
+    cursor = con.cursor()
+
+    idcheck = 'select ID from member'
+    cursor.execute(idcheck)
+    id_list = cursor.fetchall()
+    print("id_list값",id_list)
+    print("id_list의타입",type(id_list))
+    con.close()
+    return render_template('Board/join.html' , data=json.dumps(id_list, ensure_ascii=False))
 
 @app.route('/mailcheck', methods=['post'])    
 def mailcheck():
+    con = dbcall()
+    cursor = con.cursor()
+
     value = request.form
     print(value)
     print(value['email'])
     print(value['code'])
-
+    cursor.close()
     return jsonify(result = "success")
 
 
 @app.route('/join_proc', methods=['GET','POST'])
 def join_proc():
     Idexp = re.compile('^[a-zA-Z0-9]{4,12}$')
-
     if request.method == 'POST':  # request객체 안에 method 기능있음(자바도 마찬가지).
         # 키값(html의 name값, 변수명은 같게 만들어 주는게 편하니 습관화)
         user_id = request.form['user_id']
@@ -351,7 +457,14 @@ def join_proc():
             flash("비밀번호를 확인하세요." '\n' " 최소 1개 이상의 소문자, 대문자, 숫자, 특수문자로 구성되어야 하며 길이는 8자리 이상이어야 합니다.")
             return render_template('Board/join.html')
         if((user_id == True) & (Idexp.match(user_id) == True)): """
-
+        con = pymysql.connect(
+        # host='localhost',
+        user='root',
+        password='java',
+        db='final_test',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+        cursor = con.cursor()
 
         idcheck = 'select count(*) from member where ID = %s'
         cursor.execute(idcheck, user_id)
@@ -367,10 +480,9 @@ def join_proc():
             sql = 'INSERT INTO member(ID, PW, email, NAME, Phone, BIRTH) VALUES(%s,%s,%s,%s,%s,%s)'
             cursor.execute(sql, (user_id, pw_hash, user_email, user_name, user_phone, user_birth, ))
             con.commit()
+            cursor.close()
             return render_template('Board/login.html', id_list=id_list, id_cnt=id_cnt)
     
-
-
 ##################### END 회원가입관련 ###############
 
 
@@ -382,6 +494,8 @@ def my_page():
 
 @app.route('/my_page_proc', methods=['GET', 'POST'])
 def my_page_proc():
+    con = dbcall()
+    cursor = con.cursor()
 
     if request.method == 'POST':  # request객체 안에 method 기능있음(자바도 마찬가지).
         # 키값(html의 name값, 변수명은 같게 만들어 주는게 편하니 습관화)
@@ -396,7 +510,7 @@ def my_page_proc():
             sql = 'UPDATE MEMBER SET PW=%s, Phone=%s WHERE ID=%s'
             cursor.execute(sql, (user_pw, user_phone, user_id, ))
             con.commit()
-
+            con.close()
             return render_template('Board/login.html')
 
 @app.route('/recent_inquiry_company')  # 활동내역 (열람기업)
